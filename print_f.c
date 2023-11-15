b19162389f2197a3c25ce1f54de7a68391b89235
NicoBoni55 <bonifrank55@gmail.com>
2023-11-15 05:34:41 -0800

    added files

---
+++ print_f.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+/* _printf -
+ *@format :
+ *
+ *return :
+ */
+int _printf(const char *format, ...)
+{
+        unsigned int i, count_s, count = 0;
+        va_list n;
+
+        va_start(n, format);
+	if (!format || (format[0] == '%' && format[1] == '\0'))
+	{
+		return (-1);
+	}
+        for (i = 0; format[i] != '\0'; i++)
+        {
+                if (format[i] != '%')
+		{
+                        _putchar(format[i]);
+                }
+		else if (format[i + 1] == 'c')
+		{
+			_putchar(va_arg(n, int));
+			i++;
+		}
+		else if (format[i + 1] == 's')
+		{
+			count_s = putts(va_arg(n, char *));
+			i++;
+			count += (count_s - 1);
+		}
+		else if (format [i + 1] == '%')
+		{
+			_putchar('%');
+		}
+		count += 1;	
+	}
+        va_end(n);
+        return (count);
+}
