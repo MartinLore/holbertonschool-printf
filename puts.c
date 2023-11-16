56006250c4cdc88ef85d544e2ee0e3230c93184b
MartinLore <martin1342morales@gmail.com>
2023-11-16 20:18:23 -0300

    Fixed betty warnings

--- puts.c
+++ puts.c
@@ -1,20 +1,24 @@
-#include <stdio.h>
 #include "main.h"
+
 /**
- *	putts - print string
- *	
- *	@c: string
- *	Return: number of bytes
+ * putts - print string
+ * @c: string
+ *
+ * Return: number of bytes
  */
 int putts(char *c)
 {
-		int count = 0;
-		if (c)
-		{
-			for (count = 0; c[count] != '\0'; count++)
-			{
-				_putchar(c[count]);
-			}
-		}
-	return (count);
+    int count = 0;
+
+    /* If the string is not NULL, print it character by character */
+    if (c)
+    {
+        for (count = 0; c[count] != '\0'; count++)
+        {
+            _putchar(c[count]);
+        }
+    }
+
+    /* Return the count of printed characters */
+    return (count);
 }
